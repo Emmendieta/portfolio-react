09cfa16a0f3263c9d77bf677826d9f88a6856d58
Emmendieta <emmendieta12@gmail.com>
2025-10-06 14:40:58 -0300

    styling login

--- frontend/src/components/body/Login/Login.jsx
+++ frontend/src/components/body/Login/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
 
             const { user, error, message } = await loginUser(email, password);
 
-            if(error || !user) {
+            if (error || !user) {
                 //SWEET Alert
                 alert(message || "Login Failed!");
                 return;
@@ -49,28 +49,30 @@ function Login() {
 
     return (
         <div id="logDivBody">
-            <div id="logDivTop">
-                <h1 id="logDivH1">Login</h1>
-            </div>
-            <div id="logDivForm">
-                <form id="logForm" onSubmit={handleLogin}>
-                    <div className="mb-3 lofFmDiv">
-                        <label htmlFor="inputEmailLogin" className="form-label">Email:</label>
-                        <input type="email" className="form-control" placeholder="example@example.com" id="inputEmailLogin" value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            onKeyPress={handleKeyPress} />
-                    </div>
-                    <div className="mb-3 logFmDiv">
-                        <label htmlFor="inputPasswordLogin" className="form-label">Password:</label>
-                        <input type="password" className="form-control" placeholder="Type your password" id="inputPasswordLogin" value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            onKeyPress={handleKeyPress} />
-                    </div>
-                    <div id="logFormDivBtns">
-                        <button className="btn btn-outline-success" type="button" id="btnGoBack" value="Home" onClick={() => navigate("/")}>Home</button>
-                        <button className="btn btn-outline-success" type="button" id="btnLoginForm" value="Login" onClick={handleLogin}>Login</button>
-                    </div>
-                </form>
+            <div id="loginDiv">
+                <div id="logDivTop">
+                    <h1 id="logDivH1">Login</h1>
+                </div>
+                <div id="logDivForm">
+                    <form id="logForm" onSubmit={handleLogin}>
+                        <div className="mb-3 lofFmDiv">
+                            <label htmlFor="inputEmailLogin" className="form-label">Email:</label>
+                            <input type="email" className="form-control" placeholder="example@example.com" id="inputEmailLogin" value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                onKeyPress={handleKeyPress} />
+                        </div>
+                        <div className="mb-3 logFmDiv">
+                            <label htmlFor="inputPasswordLogin" className="form-label">Password:</label>
+                            <input type="password" className="form-control" placeholder="Type your password" id="inputPasswordLogin" value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                onKeyPress={handleKeyPress} />
+                        </div>
+                        <div id="logFormDivBtns">
+                            <button className="btn btn-outline-success" type="button" id="btnGoBack" value="Home" onClick={() => navigate("/")}>Home</button>
+                            <button className="btn btn-outline-success" type="button" id="btnLoginForm" value="Login" onClick={handleLogin}>Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
