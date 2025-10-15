87ab9c483ab9af6af03931d0cee397609f564a36
Emmendieta <emmendieta12@gmail.com>
2025-10-15 15:19:59 -0300

    fixing some styles

--- frontend/src/components/body/Home/GeneralFields/GeneralFields.jsx
+++ frontend/src/components/body/Home/GeneralFields/GeneralFields.jsx
@@ -1,6 +1,9 @@
 import "./GeneralFields.css";
 
 function GeneralFields({ label, value, isTextArea = false, id = "" }) {
+
+    const clickHere = "Click Here";
+    const isLink = typeof value === "string" && (value.startsWith("http://") || value.startsWith("https://") || value.startsWith("www."));
     if (isTextArea) {
         return (
             <div className="fieldDiv" id={id}>
@@ -9,10 +12,21 @@ function GeneralFields({ label, value, isTextArea = false, id = "" }) {
             </div>
         );
     };
-
     return (
         <div className="fieldDiv" id={id}>
-            <h3 className="fieldLabel">{label} {value || "-"}</h3>
+            <h3 className="fieldLabel">
+                {label}{" "}
+                {isLink ? (
+                    <a
+                        href={value}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fieldLinkDark"
+                    >
+                        {clickHere}
+                    </a>
+                ) : (value || "-")}
+            </h3>
         </div>
     );
 };
--- frontend/src/components/body/Home/GeneralFieldsDark/GeneralFieldsDark.jsx
+++ frontend/src/components/body/Home/GeneralFieldsDark/GeneralFieldsDark.jsx
@@ -1,6 +1,9 @@
 import "./GeneralFieldsDark.css";
 
 function GeneralFieldsDark({ label, value, isTextArea = false, id = "" }) {
+
+    const clickHere = "Click Here";
+    const isLink = typeof value === "string" && (value.startsWith("http://") || value.startsWith("https://") || value.startsWith("www."));
     if (isTextArea) {
         return (
             <div className="fieldDivDark" id={id}>
@@ -12,7 +15,20 @@ function GeneralFieldsDark({ label, value, isTextArea = false, id = "" }) {
 
     return (
         <div className="fieldDivDark" id={id}>
-            <h3 className="fieldLabelDark">{label} {value || "-"}</h3>
+            <h3 className="fieldLabelDark">
+                {label}{" "}
+                {isLink ? (
+                    <a
+                        href={value}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fieldLinkDark"
+                    >
+                        {clickHere}
+                    </a>
+                ) : (value || "-")
+                }
+            </h3>
         </div>
     );
 };
