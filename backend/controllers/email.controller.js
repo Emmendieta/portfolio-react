5a384c40d52cae4d67946ef8ebbe3d652b22d6c1
Emmendieta <emmendieta12@gmail.com>
2025-11-01 14:57:23 -0300

    test sending mail 2

--- backend/controllers/email.controller.js
+++ backend/controllers/email.controller.js
@@ -2,21 +2,22 @@ import { sendEmailHelper } from "../helpers/sendEmailHelper.js";
 import { validateRecaptcha } from "../helpers/validateRecaptcha.js";
 
 class EmailController {
-    constructor() {};
+    constructor() { };
 
     sendEmailContactMe = async (req, res) => {
         try {
             const { name, email, message, recaptcha } = req.body;
-            if(!name || !email || !message || !recaptcha) { return res.json400("All fields are needed!"); };
-
+            if (!name || !email || !message || !recaptcha) { return res.json400("All fields are needed!"); };
+            console.log("Body recibido:", req.body);
+            console.log("Clave secreta:", process.env.RECAPTCHA_SECRET_KEY ? "OK" : "Falta");
             const isHuman = await validateRecaptcha(recaptcha);
-            if(!isHuman) { return res.json400("Recaptcha validation failed!"); };
+            if (!isHuman) { return res.json400("Recaptcha validation failed!"); };
             const result = await sendEmailHelper({
                 fromName: name,
                 fromEmail: email,
                 message
             });
-            if(!result.success) {
+            if (!result.success) {
                 console.error("Error in sendEmailHelpler: ", result.error);
                 return res.json500("Error sending Email!");
             };
