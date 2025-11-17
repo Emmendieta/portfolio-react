e4626070eb167c4d82b40b9b4b9603cd56a2b55c
Emmendieta <emmendieta12@gmail.com>
2025-11-17 18:39:02 -0300

    testing

--- frontend/src/components/body/Home/Home.jsx
+++ frontend/src/components/body/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 
     return (
         <>
-            <Person />
+            {/* <Person /> */}
             {/* <EducationsList />
             <WorksList />
             <LanguagesList />
